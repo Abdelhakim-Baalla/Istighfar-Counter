2359c865493a4954bb80ac586d43bb3e6eb0a530
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-03-21 10:20:48 +0000

    J'ai ajouter l'event de reset de counter qui lorsque le client clicker sur le button de reset la valeur du variable counter devra etre 0

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var counter = document.getElementById('count');
 var increment = document.getElementById('increment');
 var reset = document.getElementById('reset');
 
+
 var count = 0;
 increment.addEventListener('click', function(){
     count++;
+    counter.innerText = count;
+});
+
+reset.addEventListener('click', function(){
+    count = 0;
     counter.innerText = count;
 });
