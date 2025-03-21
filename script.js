08dd5dccca9905914661fa8d3ff9a3c5c1c4df10
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-03-21 11:17:42 +0000

    J'ai changer le design et aussi J'ai ajouter une method qui va changer le titre principale par le dikr selectionner

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var counter = document.getElementById('count');
 var increment = document.getElementById('increment');
+var  decrement = document.getElementById('decrement')
 var reset = document.getElementById('reset');
 
 
@@ -8,8 +9,22 @@ increment.addEventListener('click', function(){
     count++;
     counter.innerText = count;
 });
+decrement.addEventListener('click',function(){
+    if (count == 0){
+        return;
+    }
+    
+    count--;
+    counter.innerHTML = count;
+})
 
 reset.addEventListener('click', function(){
     count = 0;
     counter.innerText = count;
 });
+
+function setDhikr(dhikr) {
+    document.querySelector('h1').innerText = `📿 ${dhikr}`;
+    count = 0;
+    counter.innerText = count;
+}
