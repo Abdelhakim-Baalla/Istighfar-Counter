b6f90176c38838a65aa1f2146810a7c2fd15a811
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-03-21 10:17:33 +0000

    J'ai linker script js par html puis j'ai crée un counter qui va incrementer lorsque le client va clicker sur + puis la valeur de html counter va remplacer par ce counter

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var counter = document.getElementById('count');
 var increment = document.getElementById('increment');
 var reset = document.getElementById('reset');
+
+var count = 0;
+increment.addEventListener('click', function(){
+    count++;
+    counter.innerText = count;
+});
